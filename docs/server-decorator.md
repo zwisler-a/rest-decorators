ae6191ff6bdb30450b6a5f987053814619c6dfa5
Zwisler <alex-z-@web.de>
2018-12-04 07:43:09 +0100

    doc: added some docs

---
+++ docs/server-decorator.md
@@ -0,0 +1,13 @@
+# Server decorator
+
+Represents a server. Starts the server according to the config give.
+@Server takes an `ServerConfig` object
+
+```ts
+
+@Server({
+    routes: []
+})
+class TestServer {}
+
+```
--- src/interfaces/server-config.interface.ts
+++ src/interfaces/server-config.interface.ts
@@ -1,13 +1,22 @@
 import { Type } from './type.interface';
 
 export interface ServerConfig {
+    /** Application port */
     port?: number;
+    /** Where to start the application, defaults to 127.0.0.1  */
     host?: string;
+    /** log debug messages */
     debug?: boolean;
+    /** Path of the static file serve */
     staticPath?: string;
+    /** If the websocket server should start */
     startWs?: boolean;
+    /** global api prefix */
     basePath?: string;
+    /** middlewares the server uses */
     middleware?: Function[];
+    /** Services registered on the Server */
     providers?: Type<any>[];
+    /** Routes registered on the server */
     routes: Type<any>[];
 }
